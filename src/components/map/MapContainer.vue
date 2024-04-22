07370b7716244da32f5d78b5d6c50749b824316b
longtian <1569346496@qq.com>
2024-04-23 01:20:29 +0800

    提交地图组件

--- src/components/map/MapContainer.vue
+++ src/components/map/MapContainer.vue
@@ -100,6 +100,22 @@ export default {
             });
             setTimeout(() => {
                 loading.close();
+                this.$confirm('行程创建完成, 是否继续?', '提示', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    
+                    type: 'warning'
+                }).then(() => {
+                    this.$message({
+                        type: 'success',
+                        message: '删除成功!'
+                    });
+                }).catch(() => {
+                    this.$message({
+                        type: 'info',
+                        message: '已取消删除'
+                    });
+                });
             }, 2000);
         }
     },
